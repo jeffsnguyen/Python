43bc06a79f4ecfe42694cf3624527cbd67fc9eac
Jeff Nguyen <son.jeff.nguyen@gmail.com>
2020-08-21 11:49:16 -0700

    Input output lecture

---
+++ Level 1/Lecture/scratch.py
@@ -0,0 +1,12 @@
+'''Demonstrate shell input/output'''
+
+def main():
+    name = input('Name:')
+    print(name)
+
+    age = int(input('Age:'))
+    print(age +2)
+
+#########################
+if __name__ == '__main__':
+    main()
