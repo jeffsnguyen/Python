5d115dd28235cd4ceec2751f250c72f661ab30c6
Jeff Nguyen <son.jeff.nguyen@gmail.com>
2020-08-27 21:35:41 -0700

    add degree of freedom to variance, Exercise 5, Section 1.3 Functions

--- Level 1/Homework/Section 1.3 Functions/Exercise 5/variance_dof.py
+++ Level 1/Homework/Section 1.3 Functions/Exercise 5/variance_dof.py
@@ -10,16 +10,23 @@ Description: Create a function that calculates the variance of a passed-in list.
 '''
 from mean_passedin_list import mean
 
-# Return variance of a list
-def variance(list):
+# Return variance of a list, with degree of freedom
+def variance(list, degOfFreedom = 1):
     mean_list = mean(list) # Calculate mean
     l_temp = [(i - mean_list)**2 for i in list] # Initialize list of (x - mean)**2
-    return sum(l_temp) / len(list)
+    return sum(l_temp) / (len(list) - degOfFreedom)
 
 def main():
+    # Initialize a list
     l = [1, 2, 3, 8, 9, 10]
 
-    print('Variance = ' + str(variance(l)))
+    # Population Variance
+    dof = 0
+    print('Variance = ' + str(variance(l, dof)))
+
+    # Sample Variance
+    dof = 1
+    print('Variance = ' + str(variance(l, dof)))
 
 #######################
 if __name__ == '__main__':
