a8fbd89ac1971e32d744509776eedc30cbbb1543
Jeff Nguyen <son.jeff.nguyen@gmail.com>
2020-09-13 00:14:24 -0700

    - Finish Exercise 5 - Learn about class composition

--- Level_2/Homework/Section_2_2_Intermediate_Classes/Exercise_5/loan/loanpool.py
+++ Level_2/Homework/Section_2_2_Intermediate_Classes/Exercise_5/loan/loanpool.py
@@ -16,15 +16,19 @@
 # Importing packages
 from loan.loan_base import Loan
 
+
 # LoanPool Class
-class LoanPool(Loan):
+class LoanPool(object):
     def __init__(self, loans=None):
         self._loans = loans
         self._loan = [loan for loan in self._loans]
-
+        self._notional = [loan.notional for loan in self._loans]
+        self._rate = [loan.rate for loan in self._loans]
+        self._term = [loan.term for loan in self._loans]
     ##########################################################
     # Decorators to define and set values for instance variables
     # Decorator to create a property function to define the attribute loans
+
     @property
     def loans(self):
         return self._loans
@@ -37,34 +41,74 @@ class LoanPool(Loan):
     ##########################################################
     # Add instance methods
 
-    # __repr__ method to display input loans
-    def __repr__(self):
-        return str(self._loans)
-
     # Instance method
     # Get all the notional values from the loans list using list comprehension
     def unpackNotionals(self):
-        return [item[0] for item in self._loans]
+        return [loan.notional for loan in self._loans]
 
     # Instance method
     # Get all the notional values from the loans list using list comprehension
     def unpackRates(self):
-        return [item[0] for item in self._loans]
+        return [loan.rate for loan in self._loans]
 
     # Instance method
     # Get all the notional values from the loans list using list comprehension
     def unpackTerms(self):
-        return [item[0] for item in self._loans]
+        return [loan.term for loan in self._loans]
 
     # Instance method
-    # Get the total loan principals
-    def balance(self, t=0):
+    # Get the aggregate remaining loan balance for a given period
+    def totalPayments(self, t=0):
         return sum([loan.balance(t) for loan in self._loans])
 
     # Instance method
-    # Get the total loan balance for a given period
+    # Get the total loan principal
     def totalPrincipal(self):
-        return sum(self.unpackNotionals())
+        return sum(self._notional)
+
+    # Instance method
+    # Get the aggregate payment due for a given period
+    def paymentDue(self, t=0):
+        return sum([loan.monthlyPayment(t) for loan in self._loans])
+
+    # Instance method
+    # Get the aggregate interest due for a given period
+    def totalInterest(self, t=0):
+        return sum([loan.interestDue(t) for loan in self._loans])
+
+    # Instance method
+    # Get the aggregate principal due for a given period
+    def principalDue(self, t=0):
+        return self.paymentDue(t) - self.totalInterest(t)
+
+    # Instance method
+    # Get the count of loan with positive balance for a given period
+    def activeLoanCount(self, t):
+        count = 0
+        for loan in self._loans:
+            if loan.balance(t) > 0:
+                count += 1
+        return count
+
+    # Instance method
+    # Calculate Weighted Average Maturity of loans in the pool
+    def WAM(self):
+        sum_amount = self.totalPrincipal()   # assign temp variable to hold the total principal
+        # Loop to calculate weighted rate of each mortgage and add them together
+        WAM_rate = 0  # Initialize WAR rate to be 0
+        for loan in self._loans:
+            WAM_rate += loan.notional * loan.term / sum_amount
+        return WAM_rate
+
+    # Instance method
+    # Calculate Weighted Average Rate of loans in the pool
+    def WAR(self):
+        sum_amount = self.totalPrincipal()  # assign temp variable to hold the total principal
+        # Loop to calculate weighted rate of each mortgage and add them together
+        WAR_rate = 0  # Initialize WAR rate to be 0
+        for loan in self._loans:
+            WAR_rate += loan.notional * loan.rate / sum_amount
+        return WAR_rate
     ##########################################################
     # Add class methods
 
--- Level_2/Homework/Section_2_2_Intermediate_Classes/Exercise_5/test_loanpool.py
+++ Level_2/Homework/Section_2_2_Intermediate_Classes/Exercise_5/test_loanpool.py
@@ -15,6 +15,7 @@
 
 # Importing necessary packages
 from loan.loanpool import LoanPool
+from loan.loan_base import Loan
 #######################
 
 
@@ -22,35 +23,70 @@ def main():
     # Testing block 1
     # Scenario:
     #   This block will:
-    #       1. Test the init function of LoanPool
-    #
-
+    #       1. Test the totalPrincipal() method of LoanPool to get total loan principal.
+    #       2. Test the balance(t) method of LoanPool to get aggregate loan balance for a given period.
+    #       3. Test the paymentDue(t) method of LoanPool to get aggregate payment due for a given period.
+    #       4. Testing totalInterest(t) method of LoanPool to get aggregate total Interest due for a given period.
+    #       5. Testing Loanpool.principalDue(t) method to get aggregate principal due for a given period.
+    #       6. Testing Loanpool.activeLoanCount(t) method to get the count of loan with positive balance for a given
+    #           period.
+    #       7.  Testing Loanpool.WAM() to calculate the Weighted Average Maturity of loans in the pool.
+    #       8.  Testing Loanpool.WAR() to calculate the Weighted Average Rate of loans in the pool.
     ###############################################
-    # 1.1 Testing MortgageMixin class
-    # Scenario: Assign a LoanPool class variable
-    # Desire result: Print correct result as assigned
-    loans = [(100000, .050, 30), (200000, .050, 30), (150000, .050, 30)]
+    # 1.1 Testing Loanpool.totalprincipal()
+    # Scenario: Calculate the total Principal of all loans
+    loan1 = Loan(100000, .050, 30)
+    loan2 = Loan(100000, .050, 30)
+    loan3 = Loan(100000, .050, 30)
+    loan4 = Loan(100000, .050, 30)
+    loans = [loan1, loan2, loan3, loan4]
     loanpool1 = LoanPool(loans)
     print('Test block 1: LoanPool')
     print('Test 1.1')
-    print('The loans are: ', loanpool1.__repr__())
-    print()
-
-    # 1.2 Testing total loan principal
-    # Scenario: Calculate the total loan principal
-    # Desire result: Print sum of loan notionals
-    print('Test 1.2')
     print('The total loan principal is: ', loanpool1.totalPrincipal())
     print()
 
-    # 1.3 Testing total loan balance for a given period
-    # Scenario: Calculate the total loan balance for a period t
-    # Desire result: Print sum of loan balance of period t
-    print(loanpool1._loan)
-    print('Test 1.3')
-    print('The total loan principal is: ', loanpool1.balance(12))
+    # 1.2 Testing Loanpool.balance(t)
+    # Scenario: Calculate the aggregate remaining loan balances for a given period t
+    print('Test 1.2')
+    print('The total loan balance is: ', loanpool1.totalPayments(26))
     print()
 
+    # 1.3 Testing Loanpool.paymentDue(t)
+    # Scenario: Calculate the aggregate loan payment due for a period t
+    print('Test 1.3')
+    print('The aggregate loan payment due is: ', loanpool1.paymentDue(26))
+    print()
+
+    # 1.4 Testing Loanpool.totalInterest(t)
+    # Scenario: Calculate the aggregate loan interest due for a period t
+    print('Test 1.4')
+    print('The aggregate interest due is: ', loanpool1.totalInterest(26))
+    print()
+
+    # 1.5 Testing Loanpool.principalDue(t)
+    # Scenario: Calculate the aggregate loan interest due for a period t
+    print('Test 1.5')
+    print('The aggregate principal due is: ', loanpool1.principalDue(26))
+    print()
+
+    # 1.6 Testing Loanpool.activeLoanCount(t)
+    # Scenario: Calculate the aggregate number of active loan with balance > 0 for a period t
+    print('Test 1.6')
+    print('The number of active loans is: ', loanpool1.activeLoanCount(26))
+    print()
+
+    # 1.7 Testing Loanpool.WAM()
+    # Scenario: Calculate the Weighted Average Maturity of loans in the pool
+    print('Test 1.7')
+    print('The Weighted Average Maturity is: ', loanpool1.WAM())
+    print()
+
+    # 1.8 Testing Loanpool.WAR()
+    # Scenario: Calculate the Weighted Average Rate of loans in the pool
+    print('Test 1.8')
+    print('The Weighted Average Maturity is: ', loanpool1.WAR())
+    print()
     ###############################################
 #######################
 if __name__ == '__main__':
