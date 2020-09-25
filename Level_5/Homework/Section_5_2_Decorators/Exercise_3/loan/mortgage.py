b11e7e93b25c5fdbae3a467742a9559d0220f48d
Jeff Nguyen <son.jeff.nguyen@gmail.com>
2020-09-25 16:17:55 -0700

    Use memoization decorator and Timer decorator on a function

---
+++ Level_5/Homework/Section_5_2_Decorators/Exercise_3/loan/mortgage.py
@@ -0,0 +1,87 @@
+# Type: Homework
+# Level: 4
+# Section: 4.2: Logging
+# Exercise: 3
+# Description: This contains the MortgageMixin class methods
+#   Add logging statements to your Loan class. This should consist of the following:
+#       a. Anytime an exception is thrown (i.e., when an incorrect Asset type is passed-into the
+#           initialization function), log an error prior to raising the exception.
+#       b. Debug-level logs which display interim steps of calculations and return values for the Loan functions.
+#       c. Info-level logs to display things like ‘t is greater than term’ in the loan functions.
+#       d. At the point the exception is caught (in your main function) use logging.exception to display
+#           the exception in addition to a custom message.
+#       e. Add a warn log to the recursive versions of the waterfall functions (that they are expected to
+#           take a long time, so the explicit versions are recommended).
+
+
+# Importing packages
+from loan.loans import FixedRateLoan, VariableRateLoan
+from asset.asset import Asset, Car, HouseBase, Lambourghini, Lexus, Civic, PrimaryHome, VacationHome
+import logging
+# Derived classes from Loan:
+# MortgageMixin
+# Does not derive from loan, only define certain things related to the mortgage
+class MortgageMixin(object):
+    def __init__(self, notional, rate, term, home):
+        # Check if passed-in home attribute is of the HouseBase family (base or derived)
+        # Init the attribute if True, else raise value error
+        if not isinstance(home, HouseBase):
+            raise ValueError('home attribute needs to be a HouseBase type.')
+        else:
+            # invoke init function if there is a base class
+            super(MortgageMixin, self).__init__(notional, rate, term, home)
+
+    ##########################################################
+
+    ##########################################################
+    # Add instance method functionalities to MortgageMixin class
+
+    # Instance method to calculate PMI based on LTV
+
+    # Instance method to calculate PMI based on LTV
+    # Private Mortgage Insurance:
+    # PMI is an extra monthly payment that one must make to compensate for the added risk of having a Loan-to-Value
+    #   (LTV) ratio of less than 80% (in other words, the loan covers >= 80% of the value of the asset).
+    #   For example, for 100k home, mortgage > 100k -> have to pay PMI
+    # This function returns .000075 of the loan notional value if LTV > .8
+    def PMI(self, period = None):
+        # Capture step/job done to debug
+        logging.debug('Step: Calculate PMI')
+        return .000075 * self._asset.value(period) if (self._notional / self._asset.value(period)) > .8 else 0
+
+    # Instance method to calculate monthly payment.
+    # This add PMI on top of the monthly payment value in Loan.monthlyPayment()
+    # NOTE: This overrides Loan.monthlyPayment
+    def monthlyPayment(self, period=None):
+        # Capture step/job done to debug
+        logging.debug('Step: Calculate monthlyPayment')
+        return super(MortgageMixin, self).monthlyPayment(period) + self.PMI(period)
+
+    # Instance method to calculate principal due.
+    # This add PMI on top of the monthly payment value in Loan.monthlyPayment()
+    # NOTE: This overrides Loan.monthlyPayment
+    def principalDue(self, period):
+        # Capture step/job done to debug
+        logging.debug('Step: Calculate principalDue')
+        return self.monthlyPayment(period) - super(MortgageMixin, self).interestDue(period)
+    ##########################################################
+
+
+# Derived classes from MortgageMixin:
+# FixedMortgage
+# Derived from MortgageMixin and FixedRateLoan
+class FixedMortgage(MortgageMixin, FixedRateLoan):
+    pass
+# VariableMortgage
+# Derived from MortgageMixin and VariableRateLoan
+class VariableMortgage(MortgageMixin, VariableRateLoan):
+    pass
+    ##########################################################
+
+    ##########################################################
+    # Add class method functionalities
+
+    ##########################################################
+    # Add static method functionalities
+
+    ##########################################################
---
+++ Level_5/Homework/Section_5_2_Decorators/Exercise_3/test_memoize.py
@@ -0,0 +1,88 @@
+# Type: Homework
+# Level: 5
+# Section: 5.2: Decorators
+# Exercise: 3
+# Description: This contains tests for Memoization of Loan waterfall function
+#   Use your memoization decorator from the previous exercise to memoize the recursive versions
+#       of the Loan waterfall functions. Time the functions before and after; do you see a difference?
+
+# Importing necessary packages
+import logging
+#from utils.timer import Timer, Memoize
+from loan.loan_base import Loan
+from asset.asset import Asset, Car
+import datetime
+from time import time
+#######################
+# To enable PyCharm to create log file
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
+# Setting log file config
+logging.basicConfig(filename='log.txt', filemode='a',
+                    format="{levelname} {processName:<12} {message} ({filename}:{lineno})", style='{')
+#######################
+
+
+def main():
+
+    logging.getLogger().setLevel(logging.DEBUG)  # Set logging level
+    ###############################################
+
+    # Testing block
+    # Scenario:
+    #   This block will:
+    #       1. Test the interestDueRecursive with no Memoize to see how long it takes.
+    #       2. Test the intenseLoop with Timer and Memoize decorator
+    #######################
+    # Test 1
+    testNum = 'Test 1'
+    logging.info(f'{testNum}')
+    print('1. Test the interestDueRecursive with no Memoize to see how long it takes.')
+    logging.info('1. Test the interestDueRecursive with Memoize to see how long it takes.')
+
+    dT_start = '2018-08-21 12:5:30:123456'
+    dT_start = datetime.datetime.strptime(dT_start, '%Y-%m-%d %H:%M:%S:%f')
+    dT_end = '2048-09-23 1:6:30:123456'
+    dT_end = datetime.datetime.strptime(dT_end, '%Y-%m-%d %H:%M:%S:%f')
+    loan1 = Loan(100000, 0.05, dT_start, dT_end, Asset(100000))
+
+    time_start = time()
+    print('Using non-cached version')
+    print(f'interestDueRecursive at month 14th = {loan1.interestDueRecursive(14)}')
+    time_end = time()
+    print(f'Took {time_end - time_start} to run.')
+    # I'm not using Timer decorator because if I do it print the Timer everytime the recursion happens.
+    print()
+
+    logging.info(f'#######################{testNum} Completed.')
+    #######################
+
+    #######################
+    # Test 2
+    testNum = 'Test 2'
+    logging.info(f'{testNum}')
+    print('2. Test the interestDueRecursive with Memoize to see how long it takes.')
+    logging.info('2. Test the interestDueRecursive with Memoize to see how long it takes.')
+
+    time_start = time()
+    print('Using cached version')
+    print(f'interestDueRecursiveMemoize at month 14th = {loan1.interestDueRecursiveMemoize(14)}')
+    time_end = time()
+    print(f'Took {time_end - time_start} to run.')
+    # I'm not using Timer decorator because if I do it print the Timer everytime the recursion happens.
+    print()
+
+    logging.info(f'#######################{testNum} Completed.')
+
+    ## Cached version with Memoize is quicker, though it's supposed to be a lot quicker??
+    ## Not sure why the difference is marginal.
+
+    #######################
+
+    ###############################################
+
+
+#######################
+if __name__ == '__main__':
+    main()
