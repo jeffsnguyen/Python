61b2a9b682e62bd38705e5ab0f51a8cf65a5ca92
Jeff Nguyen <son.jeff.nguyen@gmail.com>
2020-09-29 23:06:20 -0700

    monte hall with multiprocessing

--- Level_6/Homework/Section_6_2_Concurrency/Exercise_1/sim_monte_hall.py
+++ Level_6/Homework/Section_6_2_Concurrency/Exercise_1/sim_monte_hall.py
@@ -21,16 +21,16 @@ from time import time
 import logging
 from montehall.player import Player
 from montehall.game import Game
-import random
 import multiprocessing
 #######################
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
 ###############################################
 # Add config of log
-logging.basicConfig(filename='log.txt', filemode='w', datefmt='%Y-%m-%d %H:%M:%S',
+logging.basicConfig(filename='log.txt', filemode='a', datefmt='%Y-%m-%d %H:%M:%S',
                     format="{asctime} {levelname} {processName:<12} {message} ({filename}:{lineno})", style='{')
 ###############################################
+
 
 # Function to process the iteration
 def doWork(input, output):  # 2 parameters input queue and output queue
@@ -38,39 +38,18 @@ def doWork(input, output):  # 2 parameters input queue and output queue
         try:  # this try-except is how we can know when execution is finished
             # Each loop call get playGame(), the function that play 1 iteration of the MonteHall game
             # args in this case is False, representing the Switch strat being play from the input_queue
-            playGame, args = input.get(timeout=1)
-            res = playGame(*args)  # call playGame() with the list of arguments
+            f, args = input.get(timeout=1)
+            res = f(*args)  # call playGame() with the list of arguments
             output.put(res)  # get the result from playGame() and put it on the output queue
         except:
             break
 
 
-# Play the Monte Hall game
-def playGame(stayStrat):
-    player = Player()
-    game = Game(player)
-    # Start Game
-    # Player Turn 1
-    player_firstChoice = player.firstPlayerSelect()  # Player select first door to open
-    game.get_firstPlayerSelect(player_firstChoice)  # Host save player selection
-
-    # Host Turn
-    host_choice = game.open_firstHostSelect()  # Host select first door to open
-    player.get_firstHostSelect(host_choice)  # Player save host's selection
-
-    # Player Turn 2 (final)
-    # stayStrat is a given boolean parameter
-    finalChoice = player.secondPlayerSelect(stayStrat)  # Player select second door to open
-    game.get_secondPlayerSelect(finalChoice)  # Host save player selection
-
-    return game.checkResult(finalChoice)
-
-
 ###############################################
 def main():
     # Note:
-    # Stay Strat (True): At 10,000,000 iterations took total 137.37805318832397 seconds on a single process.
-    # Switch Strat (False): At 10,000,000 iterations took 169.34680581092834 total on a single process.
+    # Stay Strat (True): At 10,000,000 iterations took total 88.8419759273529 seconds on a single process.
+    # Switch Strat (False): At 10,000,000 iterations took 116.31930994987488 total on a single process.
 
     # Set logging level
     logging.getLogger().setLevel(logging.DEBUG)
@@ -80,24 +59,33 @@ def main():
     # Test Switch Strategy using Multiprocessing
     # Playing game in a loop
     # 5 processes, each run 2,000,000 iterations for a total 10,000,000 iterations.
+    logging.info('###############################################')
     print('Test 1')
     print('Test Switch Strategy using Multiprocessing.')
+    player1 = Player()
+    game1 = Game(player1)
+    numProcess = 5
+    parentIteration = 10000000  # Total number of iterations the game should be play
+    logging.debug(f'Running {parentIteration} iterations of the game.')
 
-    random.seed()
-    numProcess = 100
-    parentIteration = 100000 # Total number of iterations the game should be play
-
+    ####################
     # Create input/output queue
     input_queue = multiprocessing.Queue()
     output_queue = multiprocessing.Queue()
 
+    input_startTime = time()
+
     for i in range(parentIteration):
         # Each item in the queue to have a tuple of a function playGame
         #   and a list of arguments (boolean value False in this case, representing Switch strat being played)
-        input_queue.put((playGame, (False,)))
+        input_queue.put((game1.playGame, (False,)))
 
+    input_endTime = time()
+    logging.debug(f'Input queue creation timing: {input_endTime - input_startTime} seconds.')
+    ####################
     total_startTime = time()
 
+    process_startTime = time()
     # Create 5 child processes
     for i in range(numProcess):  # Loop 5 times to create 5 child processes
         # target = doWork is the function for the process to call. doWork handling processing of the iterations
@@ -105,17 +93,24 @@ def main():
         p = multiprocessing.Process(target=doWork, args=(input_queue, output_queue))
         p.start()
 
+    process_endTime = time()
+    logging.debug(f'Took {process_endTime - process_startTime} to create {numProcess} processes.')
+    ####################
+
     # Create an infinite loop and monitor output queue
     resultList = []
-
+    append_startTime = time()
     while True:
-        r = output_queue.get()  # Take something off the queue, if queue has nothing, it will block (wait)
-        # until the queue has something, while other processes running in the background
-        # when it has something, add it to the list resultList = []
-        resultList.append(r)  # When done, break the loop
         if len(resultList) == parentIteration:
             break
+        else:
+            r = output_queue.get()  # Take something off the queue, if queue has nothing, it will block (wait)
+            # until the queue has something, while other processes running in the background
+            # when it has something, add it to the list resultList = []
+            resultList.append(r)  # When done, break the loop
+    append_endTime = time()
 
+    logging.debug(f'Took {append_endTime - append_startTime} to append to list.')
     total_endTime = time()
 
     print(f'Final result list has {len(resultList)} items.')
