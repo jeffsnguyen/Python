8de22d9aeecf8964fc34eb10076307a58d631397
Jeff Nguyen <son.jeff.nguyen@gmail.com>
2020-10-04 01:53:24 -0700

    Reimplemented correctly: - increaseTimePeriod - makePrincipalPayment - makeInterestPayment
    Implemented new:
    - notionalBalance
    - interestDue
    - reset

--- Level_7/Homework/Case_Study_ABS_Modeling/spv/tranches.py
+++ Level_7/Homework/Case_Study_ABS_Modeling/spv/tranches.py
@@ -10,10 +10,11 @@
 import logging
 from spv.tranche_base import Tranche
 from utils.called_once import calledOnce
-#######################
+from utils.memoize import Memoize
 
 #######################
 
+#######################
 
 
 # Standard Tranche class
@@ -23,7 +24,11 @@ class StandardTranche(Tranche):
         # Invoke base class init
         super(StandardTranche, self).__init__(notional, rate, timePeriod, subordinationFlag)
         self._timePeriod = timePeriod
-        self._principal = 0
+        self._principalPaid = {0: 0}  # Record principal payment
+        self._interestPaid = {0: 0}  # Record interest payment
+        self._interestShortFall = {0: 0}  # Record interest shortfall
+        self._interestDue = {0: 0}  # Record interest due for each period
+        self._notionalBalance = {0: notional}  # Record notional balance owed to the tranche
 
     ##########################################################
     # Decorators to define and set values for instance variables
@@ -40,16 +45,54 @@ class StandardTranche(Tranche):
     # Record principal payment for the current tranche time period
     # Can only be called once, otherwise raised an error
     @calledOnce  # Decorator to make sure the method only called once
-    def makePrincipalPayment(self, timePeriod):
-        if not self._notional == 0:
-           return self._principal
+    def makePrincipalPayment(self, t, paidAmount):
+        if not self.notionalBalance(t) == 0:
+            self._principalPaid[t] = paidAmount
         else:
-            raise ValueError('Insufficient notional balance to make payment')
+            raise ValueError('Insufficient balance to make payment')
 
+    # Record interest payment for the current tranche time period
+    # Can only be called once, otherwise raised an error
+    # If the interest amount is less than the current interest due:
+    #   In this case, the missing amount needs to be recorded separately as an interest shortfall.
+    @calledOnce  # Decorator to make sure the method only called once
+    def makeInterestPayment(self, t, paidAmount):
+        interestDue = self.interestDue(t)
+        if not interestDue == 0:
+            self._interestShortFall[t] = interestDue - paidAmount
+            self._interestPaid[t] = paidAmount
+        else:
+            raise Exception('There is no interest due balance.')
+
+    # Return the amount of notional still owed to the tranche for the current time period (after any payments made).
+    # You can calculate this based on the original notional,
+    # the sum of all the principal payments already made, and any interest shortfalls.
+    def notionalBalance(self, t):
+        if not t == 0:
+            self._notionalBalance[t] = self._notional - sum([val for val in self._principalPaid.values()])
+        return self._notionalBalance[t]
+
+    # Return the amount of interest due for the current time period.
+    def interestDue(self, t):
+        if not t == 0:
+            self._interestDue[t] = self.notionalBalance(t-1) * self.monthlyRate(self._rate) + self._interestShortFall[t-1]
+        return self._interestDue[t]
+
+    # Reset the tranche to time t=0
+    def reset(self):
+        self._principalPaid = {0: 0}  # Record principal payment
+        self._interestPaid = {0: 0}  # Record interest payment
+        self._interestShortFall = {0: 0}  # Record interest shortfall
+        self._interestDue = {0: 0}  # Record interest due for each period
+        self._notionalBalance = {0: self._notional}  # Record notional balance owed to the tranche
     ##########################################################
     # Add class methods
 
     ##########################################################
     # Add static methods
 
+    # Calculate monthly rate based on annual rate
+    @staticmethod
+    def monthlyRate(annual_rate):
+        return annual_rate / 12
     ##########################################################
--- Level_7/Homework/Case_Study_ABS_Modeling/test_abs_model.py
+++ Level_7/Homework/Case_Study_ABS_Modeling/test_abs_model.py
@@ -52,20 +52,107 @@ def main():
     dT_start2 = datetime.datetime.strptime(dT_start2, '%Y-%m-%d %H:%M:%S:%f')
     dT_end2 = dT_start2 + datetime.timedelta(days=240)
 
-    loan1 = AutoLoan(notional=100000, rate=.08, maturity_start=dT_start1, maturity_end=dT_end1, car=Car(100000))
-    loan2 = AutoLoan(notional=75000, rate=.06, maturity_start=dT_start2, maturity_end=dT_end2, car=Car(75000))
+    rate1 = 0.08
+    rate2 = 0.06
+
+    loan1 = AutoLoan(notional=100000, rate=rate1, maturity_start=dT_start1, maturity_end=dT_end1, car=Car(100000))
+    loan2 = AutoLoan(notional=75000, rate=rate2, maturity_start=dT_start2, maturity_end=dT_end2, car=Car(75000))
     loans = LoanPool([loan1, loan2])
 
     sum_principal = loans.totalPrincipal()
-    # wA = 0.8
-    # wB = 0.2
+    print(f'Term of {loan1.__repr__()} = {loan1.term()} months')
+    print(f'Term of {loan2.__repr__()} = {loan2.term()} months')
+    print()
+
+    print(f'Monthly payment of {loan1.__repr__()} at t=0 = {loan1.monthlyPayment(0)}')
+    print(f'Monthly payment of {loan2.__repr__()} at t=0 = {loan2.monthlyPayment(0)}')
+    print(f'Monthly payment of {loan1.__repr__()} at t=10 = {loan1.monthlyPayment(10)}')
+    print(f'Monthly payment of {loan2.__repr__()} at t=8 = {loan2.monthlyPayment(8)}')
+    print(f'Monthly payment of {loan1.__repr__()} at t=11 = {loan1.monthlyPayment(11)}')
+    print(f'Monthly payment of {loan2.__repr__()} at t=9 = {loan2.monthlyPayment(9)}')
+    print()
+
+    print(f'Interest due of {loan1.__repr__()} at t=0 = {loan1.interestDue(0)}')
+    print(f'Interest due of {loan2.__repr__()} at t=0 = {loan2.interestDue(0)}')
+    print(f'Interest due of {loan1.__repr__()} at t=1 = {loan1.interestDue(1)}')
+    print(f'Interest due of {loan2.__repr__()} at t=1 = {loan2.interestDue(1)}')
+    print(f'Interest due of {loan1.__repr__()} at t=10 = {loan1.interestDue(10)}')
+    print(f'Interest due of {loan2.__repr__()} at t=8 = {loan2.interestDue(8)}')
+    print(f'Interest due of {loan1.__repr__()} at t=11 = {loan1.interestDue(11)}')
+    print(f'Interest due of {loan2.__repr__()} at t=9 = {loan2.interestDue(9)}')
+    print()
+
+    print(f'Principal due of {loan1.__repr__()} at t=0 = {loan1.principalDue(0)}')
+    print(f'Principal due of {loan2.__repr__()} at t=0 = {loan2.principalDue(0)}')
+    print(f'Principal due of {loan1.__repr__()} at t=1 = {loan1.principalDue(1)}')
+    print(f'Principal due of {loan2.__repr__()} at t=1 = {loan2.principalDue(1)}')
+    print(f'Principal due of {loan1.__repr__()} at t=10 = {loan1.principalDue(10)}')
+    print(f'Principal due of {loan2.__repr__()} at t=8 = {loan2.principalDue(8)}')
+    print(f'Principal due of {loan1.__repr__()} at t=11 = {loan1.principalDue(11)}')
+    print(f'Principal due of {loan2.__repr__()} at t=9 = {loan2.principalDue(9)}')
+    print()
+
+    print(f'Total Payments of {loan1.__repr__()} = {loan1.totalPayments()}')
+    print(f'Total Payments of {loan2.__repr__()} = {loan2.totalPayments()}')
+    print()
+
+    print(f'Total Interests of {loan1.__repr__()} = {loan1.totalInterest()}')
+    print(f'Total Interests of {loan2.__repr__()} = {loan2.totalInterest()}')
+    print()
+
+    print(f'Balance of {loan1.__repr__()} at t=0 = {loan1.balance(0)}')
+    print(f'Balance of {loan2.__repr__()} at t=0 = {loan2.balance(0)}')
+    print(f'Balance of {loan1.__repr__()} at t=1 = {loan1.balance(1)}')
+    print(f'Balance of {loan2.__repr__()} at t=1 = {loan2.balance(1)}')
+    print(f'Balance of {loan1.__repr__()} at t=9 = {loan1.balance(9)}')
+    print(f'Balance of {loan2.__repr__()} at t=7 = {loan2.balance(7)}')
+    print(f'Balance of {loan1.__repr__()} at t=10 = {loan1.balance(10)}')
+    print(f'Balance of {loan2.__repr__()} at t=8 = {loan2.balance(8)}')
+    print()
+
+    print(f'Active loans at time t=0 = {loans.activeLoanCount(0)}')
+    print(f'Active loans at time t=8 = {loans.activeLoanCount(8)}')
+    print(f'Active loans at time t=9 = {loans.activeLoanCount(9)}')
+    print(f'Active loans at time t=11 = {loans.activeLoanCount(11)}')
+    print()
+
+    print(f'Total payment at time t=0 = {loans.totalPayments(0)}')
+    print(f'Total payment at time t=7 = {loans.totalPayments(7)}')
+    print(f'Total payment at time t=9 = {loans.totalPayments(9)}')
+    print(f'Total payment at time t=10 = {loans.totalPayments(10)}')
+    print()
+
+    print(f'Total principal = {loans.totalPrincipal()}')
+    print()
+
+    print(f'Payment due at time t=0 = {loans.paymentDue(0)}')
+    print(f'Payment due at time t=7 = {loans.paymentDue(7)}')
+    print(f'Payment due at time t=8 = {loans.paymentDue(8)}')
+    print(f'Payment due at time t=11 = {loans.paymentDue(11)}')
+    print()
+
+    print(f'Total Interest at time t=0 = {loans.totalInterest(0)}')
+    print(f'Total Interest at time t=1 = {loans.totalInterest(1)}')
+    print(f'Total Interest at time t=8 = {loans.totalInterest(8)}')
+    print(f'Total Interest at time t=11 = {loans.totalInterest(11)}')
+    print()
+
+    print(f'Principal due at time t=0 = {loans.principalDue(0)}')
+    print(f'Principal due at time t=1 = {loans.principalDue(1)}')
+    print(f'Principal due at time t=9 = {loans.principalDue(9)}')
+    print(f'Principal due at time t=11 = {loans.principalDue(11)}')
+    print()
+
+    print(f'{loans.totalPayments(9)}')
+    print()
+
+
+    wA = 0.8
+    wB = 0.2
     rateA = 0.05
     rateB = 0.08
-    # notionalA = sum_principal * wA
-    # notionalB = sum_principal * wB
-
-    notionalA = 100000
-    notionalB = 75000
+    notionalA = sum_principal * wA
+    notionalB = sum_principal * wB
 
     termA = 10
     termB = 8
@@ -75,10 +162,28 @@ def main():
     print(trancheA.__repr__())
     print(trancheB.__repr__())
 
-    print(type(trancheA.increaseTimePeriod))
-    print(type(trancheA.makePrincipalPayment))
-    print(trancheA.makePrincipalPayment(1))
-    print(trancheA.makePrincipalPayment(2))
+    print(f'Tranche A notional amount = {trancheA._notional}')
+    print(f'Tranche B notional amount = {trancheB._notional}')
+    print()
+
+    trancheA.makePrincipalPayment(1, 18915.8191)
+    print(f'Tranche A principaid = {trancheA._principalPaid}')
+    print()
+
+    print(f'Tranche A notional Balance at t=0 = {trancheA.notionalBalance(0)}')
+    print(f'Tranche A notional Balance at t=1 = {trancheA.notionalBalance(1)}')
+    print()
+
+    print(f'Tranche A interest due of period t=0 = {trancheA.interestDue(0)}')
+    print(f'Tranche A interest shortfall of period t=0 = {trancheA._interestShortFall[0]}')
+    print(f'Tranche A interest due of period t=1 = {trancheA.interestDue(1)}')
+    trancheA.makeInterestPayment(1, trancheA.interestDue(1))
+    print(trancheA._principalPaid)
+
+    print(f'Tranche A interest shortfall of period t=1 = {trancheA._interestShortFall[1]}')
+    #print(f'Tranche A interest due of period t=2 = {trancheA.interestDue(2)}')
+
+    print()
 
     print()
     ###############################################
