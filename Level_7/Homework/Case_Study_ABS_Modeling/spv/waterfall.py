126ebadc3639793079dbc18f483de80aa90de3f2
Jeff Nguyen <son.jeff.nguyen@gmail.com>
2020-10-07 01:54:00 -0700

    Split doWaterfall into waterfall.py for clarity

---
+++ Level_7/Homework/Case_Study_ABS_Modeling/spv/waterfall.py
@@ -0,0 +1,62 @@
+# Type: Homework
+# Level: 7
+# Section: Case Study: Asset Backed Security Modeling
+# Exercise: 1
+# Description: This contains the Waterfall function
+#   Create a standalone function called doWaterfall. This function should take two parameters: A LoanPool
+#       object and a StructuredSecurities object. The function should loop through time periods, starting from
+#       0, and keep going until the LoanPool has no more active loans (no more payments coming from the
+#       LoanPool). At each time period iteration:
+#
+#       o Increase the time period on the StructuredSecurities object (which will, in turn, increase for all
+#           the tranches).
+#       o Ask the LoanPool for its total payment for the current time period.
+#       o Pay the StructuredSecurities with the amount provided by the LoanPool.
+#       o Call getWaterfall on both the LoanPool and StructuredSecurities objects and save the info into
+#           two variables.
+#
+#   After the loop completes (when all loans in the loan pool have zero balance), return all the results saved
+#       down from the getWaterfall function (from both variables) as well as any amount left in the reserve account.
+
+# Importing packages
+import logging
+from utils.timer import Timer
+#######################
+
+#######################
+
+
+@Timer
+def doWaterfall(loans, tranches):
+    print(f'Doing work on {tranches.__repr__()}')
+
+    # Set mode Pro Rata or Sequential
+    tranches.mode('Sequential')
+
+    ledger = {}
+    reserve = {}
+    t = 0
+    while loans.activeLoanCount(t) > 0:
+        # Increase the time period on the StructuredSecurities object (which will, in turn, increase for all
+        # the tranches).
+        tranches.increaseTranchesTimePeriod()
+        t += 1
+        # Ask the LoanPool for its total payment for the current time period.
+        collections = loans.paymentDue(t)
+
+        # Ask the LoanPool for its total principal due for the current time period.
+        principalCollected = loans.principalDue(t)
+        tranches.save_principalCollected(t, principalCollected)  # Save the principal due
+
+        # Pay the StructuredSecurities with the amount provided by the LoanPool.
+        tranches.makePayments(collections)
+
+        # Call getWaterfall on both the LoanPool and StructuredSecurities objects and save the info into
+        # two variables.
+        ledger[t] = tranches.getWaterfall(t)
+        reserve[t] = tranches.reserve[t]
+
+    print(f'My ledger is:\n {ledger}')
+    print(f'My cash reserve account:\n {reserve}')
+
+    return ledger, reserve
