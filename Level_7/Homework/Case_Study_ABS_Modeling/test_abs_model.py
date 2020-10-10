9d6e1136558aa1817ee19bbd21a3aaca6f336d48
Jeff Nguyen <son.jeff.nguyen@gmail.com>
2020-10-10 02:31:32 -0700

    modified waterfall.py to call reset() properly and not relying on a global init to init StructuredSecurities.

--- Level_7/Homework/Case_Study_ABS_Modeling/test_abs_model.py
+++ Level_7/Homework/Case_Study_ABS_Modeling/test_abs_model.py
@@ -18,7 +18,8 @@ from loan.loan_base import Loan
 from utils.import_export import loansImportCSV, spvExportCSV
 from asset.asset import Car
 from spv.structured_securities import StructuredSecurities
-from spv.waterfall import doWaterfall
+from spv.waterfall import doWaterfall, simulateWaterfall
+from math import e, sqrt
 #######################
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
@@ -29,27 +30,16 @@ logging.basicConfig(filename='log.txt', filemode='a', datefmt='%Y-%m-%d %H:%M:%S
 ###############################################
 
 
-# Initialize StructuredSecurities tranches
-def tranchesInit(loans):
-    tranches = StructuredSecurities(loans.totalPrincipal())
+def calculateYield(dirrAvg, alAvg):
+    return ((7 / (1 + 0.08 * e**(-0.19 * alAvg / 12))) + (0.019 * sqrt((alAvg / 12) * dirrAvg * 100))) / 100
+
+def runMonte(loans, tranches, tolerance, nsim):
     tranches.addTranche('StandardTranche', '0.8', '0.05', '1')
     tranches.addTranche('StandardTranche', '0.2', '0.08', '2')
-    return tranches
 
-
-# Run the sequential mode Waterfall one time
-def runSequentialSim(loans):
-    tranches = tranchesInit(loans)
-    tranches.setMode('Sequential')
-    return doWaterfall(loans, tranches)
-
-
-# Run the Pro Rata mode Waterfall one time
-def runProRataSim(loans):
-    tranches = tranchesInit(loans)
-    tranches.setMode('Pro Rata')
-    return doWaterfall(loans, tranches)
-
+    while True:
+        ledger, reserve, r, dirrAvg, dirrLetter, alAvg = simulateWaterfall(loans, tranches, nsim)
+        yieldVal = calculateYield(dirrAvg, alAvg)
 
 ###############################################
 def main():
@@ -65,45 +55,65 @@ def main():
     #   3. Do 2. in Pro Rata mode, exporting to 'liabilities_prorata.csv'
     ###############################################
     # Set logging level
-    logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger().setLevel(logging.ERROR)
     testNum = 0
 
-    # Init test variables
+    # Init test loans variables
     loan1 = AutoLoan(notional=100000, rate=0.08, term=10, car=Car(100000))
     loan2 = AutoLoan(notional=75000, rate=0.06, term=8, car=Car(75000))
     loans = LoanPool([loan1, loan2])
     loans1500 = LoanPool(loansImportCSV('Loans.csv'))
+
+    tranches = StructuredSecurities(loans.totalPrincipal())
+    tranches.addTranche('StandardTranche', '0.8', '0.05', '1')
+    tranches.addTranche('StandardTranche', '0.2', '0.08', '2')
     ###############################################
     print()
     testNum += 1
     print(f'Test {testNum}: Manual small sample run of the Waterfall on Sequential mode')
 
     print(f'Sequential mode:')
-    ledger, reserve = runSequentialSim(loans)
+    tranches.setMode('Sequential')
+    ledger, reserve, r, dirr, dirrLetter, al = simulateWaterfall(loans, tranches, 1)
     print(f'My ledger is:\n{ledger}')
     print(f'My reserve account is:\n{reserve}')
+    for tranche in tranches.tranches:
+        print(f'{tranche}\nIRR = {r}\nDIRR = {dirrLetter}\nAL = {al}')
     print()
     print(f'Pro Rata mode:')
-    ledger, reserve = runProRataSim(loans)
+    tranches.setMode('Pro Rata')
+    ledger, reserve, r, dirr, dirrLetter, al = simulateWaterfall(loans, tranches, 1)
     print(f'My ledger is:\n{ledger}')
     print(f'My reserve account is:\n{reserve}')
+    for tranche in tranches.tranches:
+        print(f'{tranche}\nIRR = {r}\nDIRR = {dirrLetter}\nAL = {al}')
     ###############################################
 
+    # Re-init StructuredSecurities to take on bigger loan pool
+    tranches = StructuredSecurities(loans1500.totalPrincipal())
+    tranches.addTranche('StandardTranche', '0.8', '0.05', '1')
+    tranches.addTranche('StandardTranche', '0.2', '0.08', '2')
     ###############################################
     print()
     testNum += 1
-    print(f'Test {testNum}: Manual small sample run of the Waterfall, sequential mode')
-    tranchesLedger, tranchesReserve = runSequentialSim(loans1500)
-    spvExportCSV(tranchesLedger, 'liabilities_sequential.csv')
+    print(f'Test {testNum}: Manual large sample run of the Waterfall, sequential mode')
+    tranches.setMode('Sequential')
+    ledger, reserve, r, dirr, dirrLetter, al = simulateWaterfall(loans1500, tranches, 1)
+    spvExportCSV(ledger, 'liabilities_sequential.csv')
+    for tranche in tranches.tranches:
+        print(f'{tranche}\nIRR = {r}\nDIRR = {dirrLetter}\nAL = {al}')
     # Answer: Each tranches did get successfully paid down to 0 and there is money left at the end.
     ###############################################
 
     ###############################################
     print()
     testNum += 1
-    print(f'Test {testNum}: Manual small sample run of the Waterfall, pro rata mode')
-    tranchesLedger2, tranchesReserve2 = runProRataSim(loans1500)
-    spvExportCSV(tranchesLedger2, 'liabilities_prorata.csv')
+    print(f'Test {testNum}: Manual large sample run of the Waterfall, pro rata mode')
+    tranches.setMode('Pro Rata')
+    ledger, reserve, r, dirr, dirrLetter, al = simulateWaterfall(loans1500, tranches, 1)
+    spvExportCSV(ledger, 'liabilities_prorata.csv')
+    for tranche in tranches.tranches:
+        print(f'{tranche}\nIRR = {r}\nDIRR = {dirrLetter}\nAL = {al}')
     # Answer: Each tranches did get successfully paid down to 0 and there is money left at the end.
     ###############################################
 
