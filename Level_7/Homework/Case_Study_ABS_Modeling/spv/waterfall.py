142782da7f4982d06eab1c60db70f6511ef4a001
Jeff Nguyen <son.jeff.nguyen@gmail.com>
2020-10-11 03:16:28 -0700

    - Rewrote simulateWaterfall() to correctly recorded metrics for each tranche
    - Modified metrics recording portion of doWaterfall() to record in tuples
    
    - Added runMonte() and other calculations

--- Level_7/Homework/Case_Study_ABS_Modeling/spv/waterfall.py
+++ Level_7/Homework/Case_Study_ABS_Modeling/spv/waterfall.py
@@ -24,33 +24,104 @@ from utils.timer import Timer
 from spv.tranche_base import Tranche
 from spv.structured_securities import StructuredSecurities
 from utils.called_once import calledOnce
-#######################
+from math import e, sqrt
+from utils.import_export import spvExportCSV
 
 #######################
 
+#######################
+
+
+def calculateDiff(nA, nB, lastARate, lastBRate, newARate, newBRate):
+    return (nA * abs((lastARate - newARate) / lastARate) + nB * abs((lastBRate - newBRate) / lastBRate)) / (nA + nB)
+
+
+def getNewRate(oldTrancheRate, coeff, yieldVal):
+    return oldTrancheRate + coeff * (yieldVal - oldTrancheRate)
+
+
+def calculateYield(dirrAvg, alAvg):
+    return ((7 / (1 + 0.08 * e**(-0.19 * alAvg / 12))) + (0.019 * sqrt((alAvg / 12) * dirrAvg * 100))) / 100
+
+
+# Run the Monte Carlo nsim number of  time
+# Each simulation:
+#   Inner simulation: calculate the average DIR and AL across all Monte Carlo scenarios (in LoanPool checkDefaults())
+#   Outer simulation: calculate yield from specific yield curve (using DIRR and AL from inner sim) to arrive at
+#       a new rate for each tranches.
+# Rinse and repeat until the yield curve converges
+@Timer
+def runMonte(loans, tranches, tolerance, nsim):
+    #tranches.addTranche('StandardTranche', '0.8', '0.05', '1')
+    #tranches.addTranche('StandardTranche', '0.2', '0.08', '2')
+
+    coeffList = [1.2, 0.8]  # 1.2 for A and 0.8 for B, list is in descending subordination order
+
+    while True:
+        oldTrancheRate = [tranche.rate for tranche in tranches.tranches]
+        ledger, reserve, tranchesMetrics = simulateWaterfall(loans, tranches, nsim)  # Save down result
+        # Calculate yield
+        # Return a list of yield, each list item represents a yield for a tranche
+        yieldVal = [calculateYield(tranche[0], tranche[1]) for tranche in tranchesMetrics]
+        newTrancheRate = [getNewRate(oldTrancheRate[i], coeffList[i], yieldVal[i]) for i, tranche in enumerate(oldTrancheRate)]
+        trancheNotional = [tranche.notional for tranche in tranches.tranches]
+
+        diff = calculateDiff(trancheNotional[0], trancheNotional[1],
+                             oldTrancheRate[0], oldTrancheRate[1],
+                             newTrancheRate[0], newTrancheRate[1])
+
+        # If diff <= tolerance, finish Monte Carlo and break the loop
+        # If diff > tolerance, reassign tranche rate to newly calculate rate and loop again.
+        if diff <= tolerance:
+            break
+
+        elif diff > tolerance:
+            for i, tranche in enumerate(tranches.tranches):
+                tranche.rate = newTrancheRate[i]
+
+    print(f'Monte Carlo simulation for yield converge completed.')
+    for tranche in tranches.tranches:
+        print(f'{tranche}\n'
+              f' IRR = {tranche.r}\n'
+              f' DIRR = {tranche.dirr}\n'
+              f' DIRR(letter) = {tranche.dirrLetter} \n'
+              f' AL = {tranche.al}')
+    return ledger
 
 # Run Waterfall nsim time
 def simulateWaterfall(loans, tranches, nsim):
-    dirrList = []
-    alList = []
+    # Init 2 tuples of 2 list, each list store a tranche metric, in descending subordination order
+    dirrList = ([], [])
+    alList = ([], [])
+
     for i in range(nsim):
-        ledger, reserve, r, dirr, dirrLetter, al = doWaterfall(loans, tranches)
-        dirrList.append(dirr)
-        if not al == None:
-            alList.append(al)
+        logging.error(f'Currently running simulation # {i}')
+        ledger, reserve, metrics = doWaterfall(loans, tranches)
 
-    dirrAvg = sum(dirrList) / len(dirrList)
-    alAvg = sum(alList) / len(alList)
-    return ledger, reserve, r, dirrAvg, dirrLetter, alAvg
+        # Save the metrics from doWaterfall() result.
+        # Metrics is a list of n tuples of 4, each tuple represents a tranche, in descending subordination order
+        # Tuple values: (irr, dirr, dirr letter rating, and al)
+        for j, tranche in enumerate(metrics):
+            dirrList[j].append(tranche[1])
+            if None not in tranche:
+                alList[j].append(tranche[3])
+
+    # Creating a zip element of n tuples,
+    #   Each tuple contains 2 lists: 1 of DIRR values and 1 of AL values, in this order
+    # Create a list of n tuple, each tuple has 2 elements representing: (average DIRR, averageAL) for each tranche
+    #   Data from the zip elements
+    tranchesMetrics = \
+        [(sum(tranche[0])/len(tranche[0]), sum(tranche[1])/len(tranche[1])) for tranche in zip(dirrList, alList)]
+
+    return ledger, reserve, tranchesMetrics
 
 
-@Timer
 def doWaterfall(loans, tranches):
     [tranche.reset() for tranche in tranches.tranches]
     tranches.reset()
 
-    print(f'Doing work on {tranches.__repr__()}')
-    logging.debug(f'Doing work on {tranches.__repr__()}')
+    #print(f'Doing work on {tranches.__repr__()}')
+    #logging.debug(f'Doing work on {tranches.__repr__()}')
 
     ledger = [tranches.getWaterfall(0)]
     reserve = []
@@ -76,10 +147,12 @@ def doWaterfall(loans, tranches):
         ledger.append(tranches.getWaterfall(t))
         reserve.append(tranches.reserve[t])
 
+    # For each tranches, save its metrics as a tuple of 4 values
+    # Method is to call these tranche's methods. Calling these methods also save the metric in the tranche's params
+    # For a 2 tranches securities, the metrics list will be a list of 2 tuples, each tuples has 4 values
+    # The metrics to be saved are: IRR, DIRR, letter DIRR rating, and AL
+    metrics = []
     for tranche in tranches.tranches:
-        r = tranche.IRR()
-        dirr = tranche.DIRR()
-        dirrLetter = tranche.getRating(dirr)
-        al = tranche.AL()
+        metrics.append((tranche.IRR(), tranche.DIRR(), tranche.DIRRLetter(), tranche.AL()))
 
-    return ledger, reserve, r, dirr, dirrLetter, al
+    return ledger, reserve, metrics
