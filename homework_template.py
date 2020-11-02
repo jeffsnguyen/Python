912571638fe7321bacab1996fb3c9dd9908a02b3
Jeff Nguyen <son.jeff.nguyen@gmail.com>
2020-11-02 01:53:37 -0800

    add some code to calculate factorial and n choose k
    for baruch's probability class

--- homework_template.py
+++ homework_template.py
@@ -1,14 +1,28 @@
-'''
-Type: Homework
-Level: 1
-Section: 1.1 Variables/ Conditionals
-Exercise: 1
-'''
+# Calculation
 
+def factorial(x):
+    i = 1
+    fac = 1
+    if x == 0:
+        return 1
+    else:
+        while i <= x:
+            fac = fac*i
+            i += 1
+    return fac
+
+
+def n_choose_k(n,k):
+    return factorial(n) // (factorial(k) * factorial(n-k))
 
 
 def main():
-    #Code goes here
+    k = 1000
+    sum = 0
+    while k <= 2000:
+        sum += ((n_choose_k(1000, 2000-k))*(n_choose_k(3000, k))/(n_choose_k(4000,2000))) * ((5000-k)/6000)
+        k += 1
+    print(f'The result is {sum}')
 
 
 
